15a783658a5e8415acbb16777c74360717f52366
Lily Sadat <l.sadatrasoul@gmail.com>
2022-06-19 12:23:36 +0430

    part3 pg1

---
+++ src/Components/Pg1Part3.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+
+export default function Pg1Part3() {
+  return (
+    <div>
+        <div className=" text-light pg1part3 pt-5 px-5" dir='rtl'>
+
+      <div>
+        <h1>آخرین دوره های آیلتس ایز</h1>
+        <h3>چرا باید آیلتس ایز را انتخاب کنم؟</h3>
+      </div>
+
+      <div className="d-flex flex-wrap mt-5">
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-taxi fs-1 ms-3"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-bar-chart ms-3 fs-1"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-envira fs-1 ms-3"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="d-flex flex-wrap py-5">
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-leanpub fs-1 ms-3"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-weixin fs-1 ms-3"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+
+        <div className="d-flex col-12 col-lg-4">
+          <div>
+            <i class="fa fa-xing-square fs-1 ms-3"></i>
+          </div>
+          <div>
+            <h3>این همان عنوان مطلب است</h3>
+            <p>
+              این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد دیگری
+              ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ کاربرد
+              دیگری ندارد. این یک متن آزمایشی برای تست جانمایی متون است و هیچ
+              کاربرد دیگری ندارد.
+            </p>
+            <button className="btn bg-none  rounded-pill ">
+            <h3><a href='' className='text-light text-decoration-none'>بیشتر بدانید »</a></h3>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar  from '../Components/Navbar'
 import Header1 from '../Components/Header1.jsx'
 import Pg1Part2 from '../Components/Pg1Part2.jsx'
-
+import Pg1Part3 from '../Components/Pg1Part3.jsx'
 
 export default function Page1() {
   return (
@@ -10,6 +10,7 @@ export default function Page1() {
          <Navbar/>
          <Header1/>
          <Pg1Part2/>
+         <Pg1Part3/>
     </div>
   )
 }
